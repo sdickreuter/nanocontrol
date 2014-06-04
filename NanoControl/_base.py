3bee473ec42a01416735debae5f5cec0b6b9656d
Simon Dickreuter <Simon.Dickreuter@uni-tuebingen.de>
2014-06-04 11:43:48 +0200

    bla

--- NanoControl/_base.py
+++ NanoControl/_base.py
@@ -9,27 +9,32 @@ class NanoControl(object):
     _x = 0
     _y = 0
 
-    def __init__(self):
+    def __init__(self, port=None):
         try:
-            self._serial = serial.Serial(d.DEFAULT_SERIAL, d.DEFAULT_BAUDRATE, timeout=1)
-        finally:
+            if port is None:
+                self._serial = serial.Serial(d.DEFAULT_SERIAL, d.DEFAULT_BAUDRATE, timeout=1)
+            else:
+                self._serial = serial.Serial(port, d.DEFAULT_BAUDRATE, timeout=1)
+        except:
             self._serial.close()
             raise RuntimeError('Could not open serial connection')
 
-        if self._serial == None:
+        if self._serial is None:
             raise RuntimeError('Could not open serial connection')
 
-        print('NanoControl initialized')
-        self._serial.write('version')
-        print('Firmware Version: ' + self._serial.readline())
-        self._serial.write('builddate')
-        print('Build Date: ' + self._serial.readline())
+        print('NanoControl initialized on port %s' %self._serial.name)
+        self._serial.write('version\r')
+        print('Firmware Version: ' + self._read_return_status())
+        self._x, self._y = self._counterread()
 
 
     def _read_return_status(self):
         buf = self._serial.readline()
-        print (buf)
-        return 0
+        buf = buf.split("\t")
+        if buf[0] == 'e':
+           raise RuntimeError('Return Status reported an error')
+
+        return buf[1]
 
     def _coarse(self, channel, steps):
         pass
@@ -49,32 +54,40 @@ class NanoControl(object):
     def _fine_reset(self, channel):
         pass
 
-    def _moveabs(self,x=None,y=None,channel=None,pos=None):
-        if (x != None) & (y != None):
-            self._serial.write('moveabs '+str(x)+' '+str(y))
-        elif (channel in ('A','B')) & (pos != None):
-            self._serial.write('moveabs '+channel+' '+str(pos))
+    def _moveabs(self, x=None, y=None, channel=None, pos=None):
+        if (x is not None) & (y is not None):
+            self._serial.write('moveabs '+str(x)+' '+str(y)+'\r')
+        elif (channel in ('A','B')) & (pos is not None):
+            self._serial.write('moveabs '+channel+' '+str(pos)+'\r')
         return self._read_return_status
 
-    def _moverel(self,dx=None,dy=None):
+    def _moverel(self, dx=None, dy=None):
         x, y = self._counterread()
         self._moveabs(x=x+dx,y=y+dy)
-        return 0
+        return self._read_return_status()
 
     def _counterread(self):
         """
         return position in nm
         """
-        self._serial.write('counterread')
-        print(self._serial.readlines())
-        return (0,0)
+        self._serial.write('counterread\r')
+        buf = self._read_return_status()
+        buf = buf.split(' ')
+        return int(buf[0]), int(buf[1])
 
     def _counterreset(self):
-        pass
+        """
+        resets all position counters
+
+        :return: return status, values of the counters
+        """
+        self._serial.write('counterreset\r')
+        return self._read_return_status()
+
 
     def home(self):
         self._moveabs(x=-200000,y=-200000)
         self._counterreset()
         self._moveabs(x=1000,y=1000)
         time.sleep(0.2)
-        self._counterreset()
+        print(self._counterreset())
--- test/stagetest.py
+++ test/stagetest.py
@@ -1,8 +1,28 @@
 __author__ = 'sei'
 
 import NanoControl as nano
-
+import time
+import math
+from datetime import datetime
 
 nc = nano.NanoControl()
 
-nc.home()
+#nc.home()
+
+A = 5000 # amplitude in nm
+T = 10 # cycle duration in s
+n = 100  # number of steps per cycle
+duration = 10000 # duration in ms
+
+def millis():
+    dt = datetime.now() - starttime
+    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+    return ms
+
+t = 0
+starttime = datetime.now()
+while (t < duration):
+        t = millis()
+        #print( int(A*math.sin(2*math.pi/T*t)))
+        print(nc._moverel(0,int(A*math.sin(2*math.pi/T*t))))
+        #time.sleep(T/n)
